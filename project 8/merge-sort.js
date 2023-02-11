0a3afb0cc4fc2ac899a9489e5923b63a29efa7d2
Arzu Caner <arzuguneycaner@gmail.com>
2023-02-11 16:50:46 +0000

    Add mergesortArraySplitAndView

--- project 8/merge-sort.js
+++ project 8/merge-sort.js
@@ -37,7 +37,46 @@ let mergesort = (
     return sorted;
 };
 
+let mergesortArraySplit = (
+    array,
+    compare = defaultCompare = 
+) => {
+    if (array.length <= 1) {retuen array.toArray(); }
+
+
+// Divide
+let pivot = Math.floor(array/length / 2);
+
+// Conquer
+let left = mergeArraySplit(array.slice(0, pivot), compare);
+let right = mergesortArraySplit(array.slice(pivot, array.length), compare);
+
+// Combine
+let sorted = [];
+let i = 0, j = 0;
+for (let k = 0; k <array.length; k++ {
+    if (i < left.length && compare(left[i]) <= 0) {
+        sorted.push(left[i]);
+        i++;
+    } else {
+        sorted.push(right[j]);
+        ji++;
+    }
+}
+
+return sorted
+}
+
 let mergesortArraySplitAndView = (array, ...args) => 
     mergesortArraySplit(ArrayView(array),...args);
+
+let mergesortWithQueue = (
+    array,
+    compare = defaultCompare
+) => {
+    if (array.length <= 1) {return array;}
+
+    // Divide
+    let pivot = Math.floor(array.length / 2);
     
 export default mergesortArraySplitAndView;
