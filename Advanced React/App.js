b11dce2c73cbfd770c15a42045f80905a9be51b5
Arzu Caner <arzuguneycaner@gmail.com>
2023-03-15 00:03:34 +0000

    Add package.json

--- Advanced React/App.js
+++ Advanced React/App.js
@@ -7,3 +7,24 @@ export default class App extends React.Component {
         )
     }
 }
+
+//* const [goOut, setGoOut] = React.useState("Yes")
+
+state = {
+    goGoOut() {
+        setGoOut(prevState => {
+            return prevState === "Yes" ? "No" : "Yes"
+        })       
+    }
+
+     render () {
+        return (
+        <div className="state">
+            <h1 className="state--title">Should I go out tonight?</h1>
+            <div className="state--value" onClick={this.toggleGoOut}>
+                <h1>{this.state.goOut}</h1>
+            </div>
+        </div>
+        )
+    }
+}
