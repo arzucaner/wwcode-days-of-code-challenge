0605bfd4695a9ab4a32594d01587804d3ddc0d05
Arzu Caner <arzuguneycaner@gmail.com>
2023-03-18 17:40:29 +0000

    Add WindowTracker.js

--- Advanced React/App.js
+++ Advanced React/App.js
@@ -151,3 +151,29 @@ componentDidMount(){
         </>
     )
  }
+
+ // Lifecycle methods: componentWillUnmount()
+
+ import React from "react"
+ import WindowTracker from "./WindowTracker"
+
+ export default class App extends React.Component {
+    state = {
+        show: true
+    }
+
+    toggle = () => {
+        this.setState(prevState => ({show: !prevState.show}))
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <button onClick={this.toggle}>
+                    Toggle WindowTracker
+                </button>
+                {this.state.show && <WindowTracker />}
+            </div>
+        )
+    }
+ }
