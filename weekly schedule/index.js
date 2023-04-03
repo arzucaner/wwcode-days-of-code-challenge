c07cbb9ebccb66280460d8be456039053856a9d2
Arzu Caner <arzuguneycaner@gmail.com>
2023-04-04 00:08:18 +0100

    Update Weekly schedule

--- weekly schedule/index.js
+++ weekly schedule/index.js
@@ -5,3 +5,36 @@ function roll(min, max, floatFlag) {
 
 let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 let firstDay = new Date("3/1/2020")
+
+function getNextDay(day) {
+    let nextDay = new Date(day)
+    nextDay.setDate(day.getDate() + 1)
+    return nextDay
+}
+
+function generateTasks() {
+    return [...Array(roll(2,5))].map((_, i) => {
+        return {
+            index: i,
+            name: `Task ${roll(1, 60)}`,
+            complete: roll(0,2) ? true : false
+        }
+    })
+}
+
+function buildWeek(day) {
+    return [...Array(7)].map((_,i) => {
+        let weekday = {
+            index: i,
+            date: day,
+            tasks: generateTasks()
+        }
+        day = getNextDay(day)
+        return weekday
+    })
+
+}
+
+console.log(buildWeek(firstDay))
+
+let week = buildWeek(firstDay)
